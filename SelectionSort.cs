756cfaaec1aee970821473caad8947fc25330a61
ivanstst <ivanstst@gmail.com>
2015-12-12 15:16:22 +0200

    Commit

---
+++ SelectionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSor
+{
+    class SelectionSort
+    {
+      public void selectSort(int [] arr)
+{
+        //pos_min is short for position of min
+	int pos_min,temp;
+         
+	for (int i=0; i < arr.Length-1; i++)
+	{
+            pos_min = i;//set pos_min to the current index of array
+		
+                for (int j=i+1; j < arr.Length; j++)
+		{
+		    if (arr[j] < arr[pos_min])
+                    {
+                        //pos_min will keep track of the index that min is in, this is needed when a swap happens
+                        pos_min = j;
+                    }                                          
+		}
+		
+        //if pos_min no longer equals i than a smaller value must have been found, so a swap must occur
+        if (pos_min != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[pos_min];
+            arr[pos_min] = temp;
+        }
+    }
+}
+
+                    }
+}
